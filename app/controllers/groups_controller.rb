de4b17a50cd0941ca905a9009faa7b62bbe6a558
otmanTR <r.brk.otman@gmail.com>
2023-06-05 13:09:22 +0300

    Add groups_controller.rb

---
+++ app/controllers/groups_controller.rb
@@ -0,0 +1,36 @@
+class FoodsController < ApplicationController
+    def index
+      @user = User.find(current_user.id)
+      @groups = @user.groups
+    end
+  
+    def new
+      @group = group.new
+    end
+  
+    def create
+      @group = current_user.groups.new(group_params)
+      @group.user = current_user
+  
+      if @group.save
+        flash[:success] = 'group created successfully.'
+        redirect_to '/groups'
+      else
+        flash[:danger] = 'group could not be created.'
+        render :new
+      end
+    end
+  
+    def destroy
+      @group = group.find(params[:id])
+      @group.destroy!
+      flash[:notice] = 'group has been successfully deleted!'
+      redirect_to groups_path
+    end
+  
+    private
+  
+    def group_params
+      params.require(:group).permit(:name, :icon)
+    end
+  end
