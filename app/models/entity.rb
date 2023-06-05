52f7f0ce3ac52b786d83c93c99179eecb51ff56e
otmanTR <r.brk.otman@gmail.com>
2023-06-05 11:38:19 +0300

    Add group model

---
+++ app/models/entity.rb
@@ -0,0 +1,8 @@
+class Entity < ApplicationRecord
+    validates :name, presence: true
+    validates :amount, presence: true
+
+    belongs_to :author, class_name: 'User'
+    has_many :groups, through: :group_entities
+
+end
